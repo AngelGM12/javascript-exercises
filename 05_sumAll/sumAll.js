9a709117cac380b8f7c4b91ca70faaab66cf38d7
AngelGM12 <angel.gutierrez74@hotmail.com>
2022-03-03 23:33:47 -0700

    completed 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    if(num1<0 || num2<0){
+        //console.log("ERROR");
+        return "ERROR"
+    }
+    
+    if(num2<num1){
+        for(let i = num2; i<=num1; i++){
+        sum += i;
+        }
+    }
+    if(typeof num1 != 'number'||typeof num2 != 'number'){
+        //console.log("ERROR");
+        return "ERROR"
+    }
+    else 
+    for(let i = num1; i<=num2; i++){
+        sum += i;
+    }
+    //console.log(sum);
+    return sum;
 };
 
 // Do not edit below this line
