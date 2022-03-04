2d81b2b6838905a01d3f50abe2b0790db3cf54df
AngelGM12 <angel.gutierrez74@hotmail.com>
2022-03-03 23:43:28 -0700

    Completed 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,15 @@
-const leapYears = function() {
-
-};
-
+const leapYears = function(year){
+  
+    //console.log(year%4);
+    if(year%4 == 0 && year%100 != 0 || year%400==0){
+      return true;
+      //console.log("true");
+    }  
+    else //console.log("false");
+    return false;
+    
+    //console.log((year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0));
+  };
+  
 // Do not edit below this line
 module.exports = leapYears;
