d9cdf9ee51b011367378e12d1fd1ab758c68a692
AngelGM12 <angel.gutierrez74@hotmail.com>
2022-03-03 23:48:18 -0700

    Completed 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(far) {
+  let c = (far-32)*(5/9);
+  c = Math.round(c * 10) / 10;
+  //console.log(c);
+  return c;
 };
 
-const ctof = function() {
-
+const ctof = function(cel) {
+  let f = cel* (9/5) +32;
+  f = Math.round(f * 10) / 10;
+  //console.log(f);
+  return f;
 };
 
 // Do not edit below this line
