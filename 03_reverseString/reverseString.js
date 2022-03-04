4607b5f8529a7f6e44c9f9a2cf22bf01cd0e5023
AngelGM12 <angel.gutierrez74@hotmail.com>
2022-03-03 23:01:07 -0700

    Compelted 1,3,4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(newstr) {
+    let strres='';
+    
+    for(let i=newstr.length-1; i>=0;i--){
+      //console.log(newstr[i])
+      strres += newstr[i];
+      
+    }
+    //console.log(strres);
+    return strres;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,25 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    /*
+  for (let c in arr){
+    console.log(arr[c]);
+  }*/
+  //console.log(args);
+  //console.log(arr);
+  
+  for(let i = 0; i< arr.length; i++){
+    //console.log(arr[i]);
+    for(let j = 0; j< args.length; j++){
+      //console.log("i "+i+"--"+ arr[i]);
+      //console.log("j ---"+j+"--"+args[j]);
+      if(arr[i]===args[j]){
+        //console.log("match");
+        arr.splice(i,1);
+        i--;
+      }
+    }
+  }
+  //console.log(arr);
+  return arr;
 };
 
 // Do not edit below this line
