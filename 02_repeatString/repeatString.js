5bbad68b852e5c897e819e997835bd54e662faa2
AngelGM12 <angel.gutierrez74@hotmail.com>
2022-03-03 23:58:39 -0700

    Completed 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let newstr = "";
+    if(num<0){
+        return "ERROR"
 
+    }
+    if(str ===''){
+        newstr = '';
+        return newstr;
+    }
+    else
+    for(let i = 0; i<num;i++){
+        newstr += str;
+    }
+    return newstr;
 };
 
 // Do not edit below this line
